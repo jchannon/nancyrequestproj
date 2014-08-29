b5bc10e53e329633afa9cb3f4a47f7478f977724
Jonathan Channon <jonathan.channon@gmail.com>
2014-08-29 16:41:48 +0100

    collection added

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -2,6 +2,7 @@
 {
     using System;
     using System.Collections.Generic;
+    using System.Linq;
 
     using Microsoft.Owin.Hosting;
 
@@ -56,6 +57,9 @@
             base.ApplicationStartup(container, pipelines);
 
             container.Register<IBlah, GlobalBlah>();
+            container.RegisterMultiple<IFoo>(Enumerable.Empty<Type>());
+
+
         }
 
         protected override void RequestStartup(TinyIoCContainer container, IPipelines pipelines, NancyContext context)
@@ -65,6 +69,10 @@
             var blah = container.Resolve<IBlah>();
             Console.WriteLine("This should be Blan NOT GlobalBlah");
             Console.WriteLine(blah.GetType());
+
+            var foo = container.ResolveAll<IFoo>();
+            Console.WriteLine("This should have 2 items");
+            Console.WriteLine("Count:"+foo.Count());
         }
     }
 
@@ -78,8 +86,26 @@
                 return new[] { new TypeRegistration(typeof(IBlah), typeof(Blah), Lifetime.PerRequest), };
             }
         }
-        public IEnumerable<CollectionTypeRegistration> CollectionTypeRegistrations { get; private set; }
+        public IEnumerable<CollectionTypeRegistration> CollectionTypeRegistrations
+        {
+            get
+            {
+                return new[] { new CollectionTypeRegistration(typeof(IFoo), new[] { typeof(Foo), typeof(Bar) }), };
+            }
+        }
         public IEnumerable<InstanceRegistration> InstanceRegistrations { get; private set; }
+    }
+
+    public class Bar : IFoo
+    {
+    }
+
+    public class Foo : IFoo
+    {
+    }
+
+    public interface IFoo
+    {
     }
 
     public class GlobalBlah : IBlah
